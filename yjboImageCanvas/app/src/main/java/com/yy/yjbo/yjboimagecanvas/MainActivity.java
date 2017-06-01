b31ecbd52c09936327b983f58232bad6c40bf499
yjbo-PC\yjbo <1457521527@qq.com>
2017-06-01 21:03:20 +0800

    基于此做矩形的四周圆角加边框的功能；

---
+++ yjboImageCanvas/app/src/main/java/com/yy/yjbo/yjboimagecanvas/MainActivity.java
@@ -0,0 +1,18 @@
+package com.yy.yjbo.yjboimagecanvas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * 基于此做矩形的四周圆角加边框的功能；
+ * @author yjbo
+ * @time 2017/6/1 20:50
+ * @qq 1457521527
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
